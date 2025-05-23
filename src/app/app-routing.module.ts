117b062f8840cd794df2734ba114bc5d7606e982
Asimbeg <asimbaig0007@gmail.com>
2025-05-23 14:26:11 +0530

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './auth.guard';
+
+const routes: Routes = [
+  {
+    path : 'register', component: SignupComponent
+  },
+  {
+    path : 'login', component: LoginComponent 
+  },
+  {
+    path : 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] 
+  }, 
+  { path: '', redirectTo: '/register', pathMatch: 'full' }, 
+    
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
