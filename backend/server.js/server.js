1f116a9ee823106ae39703d3fdcefcf0f800840b
Asimbeg <asimbaig0007@gmail.com>
2025-05-23 21:18:59 +0530

    Use process.env.PORT in server.js for deployment

--- backend/server.js/server.js
+++ backend/server.js/server.js
@@ -39,8 +39,11 @@ app.post('/signup', upload.single('profileImage'), (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 
 
